67370c6fd9564427720902afd4c83baa69b1543c
andryxatt <vlasandrey878@gmail.com>
2023-02-28 02:00:46 -0800

    add changes 2

--- src/store/user/userSlice.ts
+++ src/store/user/userSlice.ts
@@ -1,12 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {  createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../store';
-import bscTokens from "../../tokens/bsc.json";
-import polygonTokens from "../../tokens/polygon.json";
-import goerliTokens from "../../tokens/goerli.json";
-import { Contract, ethers } from 'ethers';
-import contractsAddresses from "../../contracts/AddressesContracts.json";
-import OracleAbi from "../../contracts/oracle/Oracle.json";
-import RTokenAbi from "../../contracts/RTokenAbi.json";
+
 export interface TransactionInfo {
     txHash: string;
     txStatus: string;
