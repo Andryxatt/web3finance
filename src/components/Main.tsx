3946c6ad9eca1b71ee1eefedbcb62cb2dddfcf8f
andryxatt <vlasandrey878@gmail.com>
2023-04-03 03:14:05 -0700

    changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,8 +1,6 @@
 import Table from "./table/Table";
 
 const Main = () => {
-
-
 return(
     <div className="bg-gradient-to-b from-teal-100 to-white w-full h-full rounded-2xl mt-3 pb-2 pt-3">
        <Table/>
